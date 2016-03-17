52262d74da9d57765722589cbe7edff7b885d922
needer <ssx_3000@hotmail.com>
2016-03-17 15:02:41 +0100

    Cleaned project

---
+++ src/com/example/UbonGo/UbonGo.java
@@ -0,0 +1,15 @@
+package com.example.UbonGo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UbonGo extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
